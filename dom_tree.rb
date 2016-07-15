938e3298ce09475f0d37318d420111bafacf00c6
Graham Turner <turnertgraham@gmail.com>
2016-07-15 14:57:06 -0400

    completed dom tree tests

--- dom_tree.rb
+++ dom_tree.rb
@@ -1,61 +1,54 @@
 
 class DomTree
+  attr_reader :tag
 
-
-# p = "<p class='foo bar' id='baz' name='fozzie'>"
-
-# p.match(/[a-z]/)
-
-# "<div id = 'bim'>"
-
-# "<img src='http://www.example.com' title='funny things'>"
-
-  def parse_tag(tag)
-    parsed = { "type" => tag.match(/<([a-z]*\d*)\W/).captures,
-            "classes" => tag.match(/class='(.*?)'/).captures.split,
-                 "id" => "",
-               "name" => "" }
-
-
-
-     parsed["classes"] =
-
-
-    #looks for the thing after '<' for type
-    #looks for 'class'
-    #looks for 'id'
-    #looks for 'name'
-    #sets all in a hash with keys and values -------
-  end
-
-  def classes
+  def initialize(tag)
+    @tag = tag
   end
 
   def type
-    
+    @tag.match(/<([a-z]*\d*)\W/).captures[0] if tag.match(/<([a-z]*\d*)\W/)
+  end
+
+  def classes
+    @tag.match(/class\s?=\s?'(.*?)'/).captures[0].split if tag.match(/class\s?=\s?'(.*?)'/)
   end
 
   def id
-
+    @tag.match(/id\s?=\s?'(.*?)'/).captures[0] if tag.match(/id\s?=\s?'(.*?)'/)
   end
 
   def name
-
+    @tag.match(/name\s?=\s?'(.*?)'/).captures[0] if tag.match(/name\s?=\s?'(.*?)'/)
   end
 
+  def title
+    @tag.match(/title\s?=\s?'(.*?)'/).captures[0] if tag.match(/title\s?=\s?'(.*?)'/)
+  end
 
-
-# tag = parse_tag("<p class='foo bar' id='baz' name='fozzie'>")
-# tag.type 
-# #=> "p"
-# tag.classes 
-# #=> ["foo", "bar"]
-# tag.id 
-# #=> "baz"
-# tag.name 
-# #=> "fozzie"
-
+  def src
+    @tag.match(/src\s?=\s?'(.*?)'/).captures[0] if tag.match(/src\s?=\s?'(.*?)'/)
+  end
 
 end
 
-sfsdfd
+par = DomTree.new("<p class='foo bar' id='baz' name='fozzie'>")
+d = DomTree.new("<div id = 'bim'>")
+i = DomTree.new("<img src='http://www.example.com' title='funny things'>")
+
+p par.type
+p par.classes
+p par.id
+p par.name
+
+p d.type
+p d.classes
+p d.id
+p d.name
+
+p i.type
+p i.classes
+p i.id
+p i.name
+p i.src
+p i.title
