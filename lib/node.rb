0e7089b098512ca28bfb3962dc0b87d50bc85903
chrisgoodson <f.chrisgoodson@gmail.com>
2016-07-18 16:59:42 -0500

    monkey patching node class

--- lib/node.rb
+++ lib/node.rb
@@ -1,1 +1,16 @@
-Node = Struct.new(:type, :parent, :depth, :children )
+Node = Struct.new(:type, :parent, :depth, :children, :attributes)
+
+
+class Node
+
+  def self.build_attributes_hash
+    self.attributes << self.
+
+
+  end 
+
+
+
+
+
+end
